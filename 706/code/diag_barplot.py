7d1d226fdd61975dc3957b5607853e33727fc751
richardyzx <richardyzx@gmail.com>
2022-04-24 17:20:46 -0400

    test

--- 706/code/diag_barplot.py
+++ 706/code/diag_barplot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-df = pd.read_csv("/706/data/diags.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+df = pd.read_csv("706/data/diags.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
 
 st.write('hello world!')
 
