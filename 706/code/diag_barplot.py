c3411833737d7db270d909bbcab6ce87d5187273
richardyzx <richardyzx@gmail.com>
2022-04-27 03:21:01 -0400

    test

--- 706/code/diag_barplot.py
+++ 706/code/diag_barplot.py
@@ -1,10 +1,18 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
+
 
 
 df = pd.read_csv("706/data/diags.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
 
 st.write('hello world!')
-st.write('hello world!!')
+chart = alt.Chart(df).mark_rect().encode(
+    x=alt.X("ICD9_CODE"),
+    y='count(SUBJECT_ID)',
+    
+).properties(
+    #title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
+)
 
 
