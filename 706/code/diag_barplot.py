047ca8b58ffef38cf8e60964a8a571ffa5af9a83
richardyzx <richardyzx@gmail.com>
2022-04-27 16:00:26 -0400

    death button

--- 706/code/diag_barplot.py
+++ 706/code/diag_barplot.py
@@ -6,7 +6,22 @@ import altair as alt
 
 df = pd.read_csv("706/data/diags.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
 
-st.write('hello world!')
+def label_death (row):
+   if row['EXPIRE_FLAG'] == 1 :
+      return 'Expired'
+   return 'Survived'
+
+df['Death'] = df.apply (lambda row: label_death(row), axis=1)
+
+
+death = st.radio(
+    "Death",
+    ('Expired', 'Survived'))
+
+subset = df[df["Death"] == death]
+
+
+
 chart = alt.Chart(df).mark_rect().encode(
     x=alt.X("ICD9_CODE"),
     y=alt.Y('count(SUBJECT_ID)', title = 'number of patients'),
