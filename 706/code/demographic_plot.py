ebbfc11676db45bf776e17223d519caae680a838
chenfeng1234567 <60585774+chenfeng1234567@users.noreply.github.com>
2022-04-28 16:58:43 -0400

    Update demographic_plot.py

--- 706/code/demographic_plot.py
+++ 706/code/demographic_plot.py
@@ -42,7 +42,7 @@ donut = alt.Chart(df).mark_arc(innerRadius=50, outerRadius=90).encode(
 	width = 250
     )
 
-bar1 & bar2.properties(data=df.sample(sdf.shape[0])
+bar1 & bar2.properties(data=df.sample(sdf.shape[0]))
 #chart = alt.vconcat(bar1, bar2.properties(data=df.sample(sdf.shape[0]))
 #).resolve_scale(
 #    color='independent'
