1090b89a44c470a4da72e6592737eeb4e3a8cd88
chenfeng1234567 <60585774+chenfeng1234567@users.noreply.github.com>
2022-04-30 16:48:00 -0400

    Update demographic_plot.py

--- 706/code/demographic_plot.py
+++ 706/code/demographic_plot.py
@@ -33,7 +33,7 @@ bar1 = base.encode(
     y = 'count(SUBJECT_ID)',
     color = alt.Color('EXPIRE_FLAG:N'),
     tooltip = ['count(SUBJECT_ID)','AGE_GROUP','EXPIRE_FLAG'],
-    column = alt.Column('AGE_GROUP', header = alt.Header(labelOrient = "bottom"))
+    #column = alt.Column('AGE_GROUP', header = alt.Header(labelOrient = "bottom"))
     ).add_selector(
 	expire_select
     ).properties(
