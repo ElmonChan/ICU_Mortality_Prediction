4f134068d24cac6e2b124366894f0f80467338dd
chenfeng1234567 <60585774+chenfeng1234567@users.noreply.github.com>
2022-04-30 16:49:43 -0400

    Update demographic_plot.py

--- 706/code/demographic_plot.py
+++ 706/code/demographic_plot.py
@@ -29,10 +29,10 @@ expire_select = alt.selection_single(fields=['EXPIRE_FLAG'], bind=expire_dropdow
 base = alt.Chart(df)
 
 bar1 = base.encode(
-    x = alt.X('AGE_GROUP',title=None, axis=alt.Axis(labels=False)),
+    x = 'AGE_GROUP',
     y = 'count(SUBJECT_ID)',
     color = alt.Color('EXPIRE_FLAG:N'),
-    #tooltip = ['count(SUBJECT_ID)','AGE_GROUP','EXPIRE_FLAG'],
+    tooltip = ['count(SUBJECT_ID)','AGE_GROUP','EXPIRE_FLAG'],
     #column = alt.Column('AGE_GROUP', header = alt.Header(labelOrient = "bottom"))
     ).add_selector(
 	expire_select
