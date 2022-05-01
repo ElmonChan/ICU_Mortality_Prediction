7e78a7427d868134e6421adbbd6b6b0337c28953
richardyzx <richardyzx@gmail.com>
2022-05-01 02:02:35 -0400

    correction

--- 706/code/lab_time_scatter.py
+++ 706/code/lab_time_scatter.py
@@ -54,8 +54,8 @@ chart2 = alt.Chart(abnormal_labs).mark_circle(size=20).encode(
     #y= alt.Y ('VALUENUM', title = f"Value ({unit})"),
 #)
 
-#chart1 + chart1.transform_regression('time_to_icu_mins', 'VALUENUM', groupby=['Survival']).mark_line()
-#chart2 + chart2.transform_regression('time_to_icu_mins', 'VALUENUM', groupby=['Survival']).mark_line()
+chart1 = chart1 + chart1.transform_regression('time_to_icu_mins', 'VALUENUM', groupby=['Survival']).mark_line()
+chart2 = chart2 + chart2.transform_regression('time_to_icu_mins', 'VALUENUM', groupby=['Survival']).mark_line()
 
 #sc_plot + sc_plot.transform_regression('temp_max', 'temp_min').mark_line()
 st.write("### lab records within normal range")
