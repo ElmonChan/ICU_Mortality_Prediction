0c4e2b9760578db513e105d8f794a0495ae300a1
ElmonChan <50758702+ElmonChan@users.noreply.github.com>
2022-05-02 22:29:50 -0400

    Update lab scatter

--- 706/code/lab_time_scatter.py
+++ 706/code/lab_time_scatter.py
@@ -12,7 +12,7 @@ def app():
     icu_labs = pd.read_csv('706/data/icu_lab.csv')
 
     labs = list(icu_labs.LABEL.unique())
-    default_ix = labs.index('Cholesterol, Total')
+    default_ix = labs.index('Albumin')
     option = st.selectbox('Select a lab to study', labs, index=default_ix)
 
     subset = icu_labs[icu_labs["LABEL"] == option]
@@ -28,31 +28,31 @@ def app():
 
     unit = subset["VALUEUOM"].iloc[0]
 
-    normal_labs = subset[subset['FLAG'] != 'abnormal']
-    abnormal_labs = subset[subset['FLAG'] == 'abnormal']
+    # normal_labs = subset[subset['FLAG'] != 'abnormal']
+    # abnormal_labs = subset[subset['FLAG'] == 'abnormal']
 
-    chart1 = alt.Chart(normal_labs).mark_circle(size=20).encode(
-        x= alt.X('time_to_icu_mins', scale=alt.Scale(reverse=True), title = 'Time Before ICU Admission (min)'),
-        y= alt.Y ('VALUENUM',  scale=alt.Scale(zero=False), title=f"Value ({unit})"),
-        color='Survival',
-        tooltip=['FLAG', 'time_to_icu_mins', 'Survival']
-    )
+    # chart1 = alt.Chart(normal_labs).mark_circle(size=20).encode(
+    #     x= alt.X('time_to_icu_mins', scale=alt.Scale(reverse=True), title = 'Time Before ICU Admission (min)'),
+    #     y= alt.Y ('VALUENUM',  scale=alt.Scale(zero=False), title=f"Value ({unit})"),
+    #     color='Survival',
+    #     tooltip=['FLAG', 'time_to_icu_mins', 'Survival']
+    # )
 
-    chart2 = alt.Chart(abnormal_labs).mark_circle(size=20).encode(
+    chart2 = alt.Chart(subset).mark_circle(size=20).encode(
         x= alt.X('time_to_icu_mins', scale=alt.Scale(reverse=True), title = 'Time Before ICU Admission (min)'),
         y= alt.Y ('VALUENUM', scale=alt.Scale(zero=False), title=f"Value ({unit})"),
         color='Survival',
         tooltip=['FLAG', 'time_to_icu_mins', 'Survival']
     )
 
-    chart1 = chart1 + chart1.transform_regression('time_to_icu_mins', 'VALUENUM', groupby=['Survival']).mark_line()
+    # chart1 = chart1 + chart1.transform_regression('time_to_icu_mins', 'VALUENUM', groupby=['Survival']).mark_line()
     chart2 = chart2 + chart2.transform_regression('time_to_icu_mins', 'VALUENUM', groupby=['Survival']).mark_line()
-    chart1 = chart1.properties(width=800, height=600)
+    # chart1 = chart1.properties(width=800, height=600)
     chart2 = chart2.properties(width=800, height=600)
 
     #sc_plot + sc_plot.transform_regression('temp_max', 'temp_min').mark_line()
-    st.write("### lab records outside normal range")
+    # st.write("### lab value")
     st.altair_chart(chart2)
-    st.write("### lab records within normal range")
-    st.altair_chart(chart1)
+    # st.write("### lab records within normal range")
+    # st.altair_chart(chart1)
 
